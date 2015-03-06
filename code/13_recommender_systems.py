cf8d1aa014aeb9139ae648ae069c65cd08bd42ca
Harish Kashyap <hkashyap@icloud.com>
2015-03-05 20:28:20 -0500

    Uploading beer recommender with yhat

--- code/13_recommender_systems.py
+++ code/13_recommender_systems.py
@@ -4,94 +4,54 @@ Created on Mon Mar  2 22:22:17 2015
 
 @author: harishkashyap
 """
-
 import pandas as pd
 import numpy as np
-import pylab as pl
+from sklearn.metrics.pairwise import cosine_similarity
+import pprint as pp
+import os
 
-df = pd.read_csv("/Users/harishkashyap/Documents/beer_reviews/beer_reviews.csv")
-df.head()
+filename = os.path.join("/Users/harishkashyap/Documents/beer_reviews/beer_reviews.csv")
+df = pd.read_csv(filename)
+# let's limit things to the top 250
+n = 250
+top_n = df.beer_name.value_counts().index[:n]
+df = df[df.beer_name.isin(top_n)]
 
-beer_1, beer_2 = "Dale's Pale Ale", "Fat Tire Amber Ale"
+print "melting..."
+df_wide = pd.pivot_table(df, values=["review_overall"],
+                         rows=["beer_name", "review_profilename"],
+                         aggfunc=np.mean).unstack()
 
-beer_1_reviewers = df[df.beer_name==beer_1].review_profilename.unique()
-beer_2_reviewers = df[df.beer_name==beer_2].review_profilename.unique()
-common_reviewers = set(beer_1_reviewers).intersection(beer_2_reviewers)
-print "Users in the sameset: %d" % len(common_reviewers)
-list(common_reviewers)[:10]
+# any cells that are missing data (i.e. a user didn't buy a particular product)
+# we're going to set to 0
+df_wide = df_wide.fillna(0)
 
-def get_beer_reviews(beer, common_users):
-    mask = (df.review_profilename.isin(common_users)) & (df.beer_name==beer)
-    reviews = df[mask].sort('review_profilename')
-    reviews = reviews[reviews.review_profilename.duplicated()==False]
-    return reviews
-beer_1_reviews = get_beer_reviews(beer_1, common_reviewers)
-beer_2_reviews = get_beer_reviews(beer_2, common_reviewers)
+# this is the key. we're going to use cosine_similarity from scikit-learn
+# to compute the distance between all beers
+print "calculating similarity"
+dists = cosine_similarity(df_wide)
 
-cols = ['beer_name', 'review_profilename', 'review_overall', 'review_aroma', 'review_palate', 'review_taste']
-beer_2_reviews[cols].head()
+# stuff the distance matrix into a dataframe so it's easier to operate on
+dists = pd.DataFrame(dists, columns=df_wide.index)
 
-# choose your own way to calculate distance
-from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.metrics.pairwise import manhattan_distances
-from scipy.stats.stats import pearsonr
+# give the indicies (equivalent to rownames in R) the name of the product id
+dists.index = dists.columns
 
+def get_sims(products):
+    """
+    get_top10 takes a distance matrix an a productid (assumed to be integer)
+    and will calculate the 10 most similar products to product based on the
+    distance matrix
 
-ALL_FEATURES = ['review_overall', 'review_aroma', 'review_palate', 'review_taste']
-def calculate_similarity(beer1, beer2):
-    # find common reviewers
-    beer_1_reviewers = df[df.beer_name==beer1].review_profilename.unique()
-    beer_2_reviewers = df[df.beer_name==beer2].review_profilename.unique()
-    common_reviewers = set(beer_1_reviewers).intersection(beer_2_reviewers)
+    dists - a distance matrix
+    product - a product id (integer)
+    """
+    p = dists[products].apply(lambda row: np.sum(row), axis=1)
+    p = p.order(ascending=False)
+    return p.index[p.index.isin(products)==False]
 
-    # get reviews
-    beer_1_reviews = get_beer_reviews(beer1, common_reviewers)
-    beer_2_reviews = get_beer_reviews(beer2, common_reviewers)
-    dists = []
-    for f in ALL_FEATURES:
-        dists.append(euclidean_distances(beer_1_reviews[f], beer_2_reviews[f])[0][0])
-    
-    return dists
-
-calculate_similarity(beer_1, beer_2)
-
-# calculate only a subset for the demo
-beers = ["Dale's Pale Ale", "Sierra Nevada Pale Ale", "Michelob Ultra",
-         "Natural Light", "Bud Light", "Fat Tire Amber Ale", "Coors Light",
-         "Blue Moon Belgian White", "60 Minute IPA", "Guinness Draught"]
-
-# calculate everything for real production
-# beers = df.beer_name.unique()
-
-simple_distances = []
-for beer1 in beers:
-    print "starting", beer1
-    for beer2 in beers:
-        if beer1 != beer2:
-            row = [beer1, beer2] + calculate_similarity(beer1, beer2)
-            simple_distances.append(row)
-            
-cols = ["beer1", "beer2", "overall_dist", "aroma_dist", "palate_dist", "taste_dist"]
-simple_distances = pd.DataFrame(simple_distances, columns=cols)
-simple_distances.tail()
-
-def calc_distance(dists, beer1, beer2, weights):
-    mask = (dists.beer1==beer1) & (dists.beer2==beer2)
-    row = dists[mask]
-    row = row[['overall_dist', 'aroma_dist', 'palate_dist', 'taste_dist']]
-    dist = weights * row
-    return dist.sum(axis=1).tolist()[0]
-
-weights = [2, 1, 1, 1]
-print calc_distance(simple_distances, "Fat Tire Amber Ale", "Dale's Pale Ale", weights)
-print calc_distance(simple_distances, "Fat Tire Amber Ale", "Michelob Ultra", weights)
-
-my_beer = "Coors Light"
-results = []
-for b in beers:
-    if my_beer!=b:
-        results.append((my_beer, b, calc_distance(simple_distances, my_beer, b, weights)))
-sorted(results, key=lambda x: x[2])
+get_sims(["Sierra Nevada Pale Ale", "120 Minute IPA", "Coors Light"])
+# Index([u'Samuel Adams Boston Lager', u'Sierra Nevada Celebration Ale', u'90 Minute IPA', u'Arrogant Bastard Ale', u'Stone IPA (India Pale Ale)', u'60 Minute IPA', u'HopDevil Ale', u'Stone Ruination IPA', u'Sierra Nevada Bigfoot Barleywine Style Ale', u'Storm King Stout', u'Samuel Adams Winter Lager', u'Samuel Adams Summer Ale', u'Prima Pils', u'Anchor Steam Beer', u'Old Rasputin Russian Imperial Stout', u'Samuel Adams Octoberfest', ...], dtype='object')
 
 from yhat import Yhat, YhatModel, preprocess
 
@@ -105,3 +65,9 @@ class BeerRecommender(YhatModel):
             result.append({"beer": beer})
         return result
 
+BeerRecommender().execute({"beers": ["Sierra Nevada Pale Ale","120 Minute IPA", "Stone Ruination IPA"]})
+
+yh = Yhat("USERNAME", "APIKEY", "http://cloud.yhathq.com")
+yh.deploy("BeerRecommender", BeerRecommender, globals())
+
+yh.predict("BeerRecommender",{"beers": ["Sierra Nevada Pale Ale","120 Minute IPA", "Stone Ruination IPA"]})
